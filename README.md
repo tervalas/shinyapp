07859207bf458a7598382f372b9e37bd5c89b111
Kristopher Lohmuller <tervalas@gmail.com>
2017-12-08 09:33:01 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Car Comparison Shiny App
+
+These files are the server.R and ui.R files for my shiny app at https://tervalas.shinyapps.io/car_comparisons/
---
+++ server.R
@@ -0,0 +1,31 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+   model <- reactive({
+       brushed_data <- brushedPoints(mtcars,input$brush1,xvar="wt",yvar="mpg")
+       if(nrow(brushed_data)<2){
+           return(NULL)
+       }
+       lm(mpg~wt, data=brushed_data)
+   })
+   output$slopeOut <- renderText({
+       if(is.null(model())){
+           "No Model Found"
+       } else {
+           model()[[1]][2]
+       }
+   })
+   output$intOut <- renderText({
+       if(is.null(model())){
+           "No Model Found"
+       } else {
+           model()[[1]][1]
+       }
+   })
+   output$plot1 <- renderPlot({
+       plot(mtcars$wt,mtcars$mpg,xlab="Weight",ylab="Miles Per Gallon",main="How Weight Affects Milage",cex=1.5,pch=16,bty="n")
+       if(!is.null(model())){
+           abline(model(),col="blue",lwd=2)
+       }
+   })
+})
